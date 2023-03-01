b16d40c867e21df4c77c47dd6144a7f3ccdb20ac
Aditya Gitte <adityagitte@Adityas-MacBook-Pro.local>
2023-03-01 14:04:41 +0530

    access token and calender API working

--- OAuth/urls.py
+++ OAuth/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest/v1/calendar/init/', views.GoogleCalendarInitView),
+    path('rest/v1/calendar/redirect/', views.GoogleCalendarRedirectView)
 ]
--- OAuth/views.py
+++ OAuth/views.py
@@ -1,6 +1,8 @@
 import urllib.parse
-
-from django.contrib.sites import requests
+import requests
+import google.oauth2.credentials
+from googleapiclient.discovery import build
+from datetime import datetime, time, timedelta
 from django.http import HttpResponseRedirect
 
 
@@ -11,7 +13,7 @@ def GoogleCalendarInitView(request):
     # Define the parameters for the authorization request
     params = {
         'client_id': "1039235666937-f8qtim0qodk5bojr9ebslqkbaitpuuj8.apps.googleusercontent.com",
-        'redirect_uri': 'http://localhost:8000/rest/v1/calendar/redirect/',
+        'redirect_uri': 'http://127.0.0.1:8000/rest/v1/calendar/redirect/',
         'response_type': 'code',
         'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
         'access_type': 'offline',
@@ -25,19 +27,22 @@ def GoogleCalendarInitView(request):
     return HttpResponseRedirect(url)
 
 
-def google_oauth_callback(request):
+def GoogleCalendarRedirectView(request):
     # Get the authorization code from the request
     code = request.GET.get('code')
 
     # Define the Google OAuth token endpoint
     token_url = 'https://accounts.google.com/o/oauth2/token'
 
+    #Print the access code as log
+    print(code)
+
     # Define the parameters for the token request
     data = {
         'client_id': '1039235666937-f8qtim0qodk5bojr9ebslqkbaitpuuj8.apps.googleusercontent.com',
         'client_secret': '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',
         'grant_type': 'authorization_code',
-        'redirect_uri': 'http://localhost:8000/google/oauth/callback',
+        'redirect_uri': 'http://127.0.0.1:8000/rest/v1/calendar/redirect/',
         'code': code
     }
 
@@ -48,3 +53,31 @@ def google_oauth_callback(request):
     token_data = response.json()
     access_token = token_data.get('access_token')
     refresh_token = token_data.get('refresh_token')
+    print(f"[LOG] The access token is {access_token}")
+    print(f"[LOG] The refresh token is {refresh_token}")
+
+    # Set up the credentials object from the access token
+    creds = google.oauth2.credentials.Credentials(access_token)
+
+    # Create a Calendar API client object
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API to retrieve the events
+    events_result = service.events().list(
+        calendarId='primary',
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+
+    # Get the events from the response object
+    events = events_result.get('items', [])
+
+    # Print the events
+    if not events:
+        print('No events found.')
+    else:
+        print('Upcoming events:')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            start_time = datetime.fromisoformat(start)
+            print(f'{event["summary"]} ({start_time.strftime("%m/%d/%Y %I:%M %p")})')
